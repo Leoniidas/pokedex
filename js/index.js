d71992865e0ad21c6d2d0c8957f3c61a331fa84d
Vinicius M <57871297+Leoniidas@users.noreply.github.com>
2023-06-06 02:10:20 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,33 @@
+//Criando a classe para usar o botao pra mudar o tema
+const botaoAlterarTema = document.getElementById("botao-alterar-tema");
+//Criando a classe body pra receber o novo tema
+const body = document.querySelector("body");
+//Criando a classe da imagem pra poder mudar de sol para lua e vice-versa
+const imagemBotaoTrocadeTema = document.querySelector(".imagem-botao");
+//Quando clicar no botao, fazer a arrowfunction para mudar os temas.
+botaoAlterarTema.addEventListener("click", () => {
+    const modoEscuroEstaAtivo = body.classList.contains("modo-escuro");
+    //Toggle para remover/adicionar a classe modo-escuro de forma que acontece sempre que clicar.
+    body.classList.toggle("modo-escuro");
+
+    if (modoEscuroEstaAtivo) {
+
+        imagemBotaoTrocadeTema.setAttribute("src", "./src/imagens/sun.png");
+    } else {
+
+        imagemBotaoTrocadeTema.setAttribute("src", "./src/imagens/moon.png");
+    }
+});
+//Botao para ir do fim da pagina para o topo
+const botao = document.querySelector(".btn-fixed");
+
+window.addEventListener("scroll", function (event) {
+    if (this.window.scrollY == 0) {
+
+        botao.classList.remove("visible");
+        
+    } else {
+
+        botao.classList.add("visible");
+    }
+})
